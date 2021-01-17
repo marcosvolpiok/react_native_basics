6eb40d2bd3c99532afcef6b4b1a65f9b4e337a14
Marcos Volpi <marcos.volpi@BP4.com>
2021-01-17 11:28:03 -0300

    Init

---
+++ components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Image, Text,StyleSheet} from 'react-native';
+
+function About(props){
+
+    return(
+        <View>
+            <Text>Hola mundo</Text>
+        </View>
+    )
+}
+
+export default About
